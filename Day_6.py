a352b5de8b2db1081791662b5a77f0065d1cf7c5
AniketBembale1008 <anibembale1004@gmail.com>
2025-05-05 23:30:01 +0530

    Day 6 Code

--- Day_6.py
+++ Day_6.py
@@ -3,4 +3,14 @@ T = int(input())
 
 for _ in range(T):
     String = input()
-    print(String)
+    print(String[0::2], String[1::2])
+
+# input
+# 2
+# Hacker
+# Rank
+
+
+# Output
+# Hce akr
+# Rn ak
